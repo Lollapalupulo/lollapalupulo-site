9badabf35ef38b350254f79d6c119af8ca8c27c3
Lollapalupulo <lollapalupulo@gmail.com>
2025-06-06 07:38:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo: Menu Mobile (para versão responsiva)
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Site carregado!');
+  // Adicione interações aqui (ex.: carrossel, accordion para programação)
+});
